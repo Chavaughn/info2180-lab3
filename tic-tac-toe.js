0966832473c01966be180e113e5c91a7b1b3f796
Chavvv <chavaughnwilkins@gmail.com>
2021-10-10 17:08:57 -0500

    No cheating and winners detected

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,14 +7,15 @@ window.addEventListener("load", (event) => {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 5, 6]
+        [2, 4, 6]
     ];
     let xWinnerVal = 0;
     let oWinnerVal = 0;
+    let x = true;
+    let o = false;
     let active;
     let squares = document.querySelectorAll("#board > div");
     let bton = document.querySelector(".btn");
-    let state = -1;
 
     squares.forEach((sq) => {
         sq.classList.add("square");
@@ -23,25 +24,30 @@ window.addEventListener("load", (event) => {
     });
     squares.forEach((sq) => {
         sq.onclick = (event) => {
-            selected = 0;
-            if (state == -1 || state == 1) {
-                sq.classList.remove("O");
+            if (x && sq.innerHTML == " ") {
                 sq.classList.add("X");
                 sq.innerHTML = 'X';
-                state = 0;
-            } else {
-                sq.classList.remove("X");
+                x = false;
+                o = true;
+            } else if (o && sq.innerHTML == " ") {
                 sq.classList.add("O");
                 sq.innerHTML = 'O';
-                state = 1;
+                o = false;
+                x = true;
             }
 
+            active = checkWinner();
 
-            while (selected <= squares.length) {
-                if (squares[selected] == sq) {
-                    break;
-                }
-                selected++;
+            if (active == 1) {
+                document.querySelector("div#status").classList.add("you-won");
+                document.querySelector("div#status").innerHTML = `Congratulations! ${sq.innerHTML} is the Winner!`;
+
+                squares.forEach((square) => {
+                    square.onclick = (event) => {
+                        event.preventDefault();
+                    }
+                })
+
             }
 
         }
@@ -54,13 +60,34 @@ window.addEventListener("load", (event) => {
         sq.onmouseout = (event) => {
             sq.classList.remove("hover");
         }
+        let checkWinner = () => {
 
+            for (item = 0; Winapprove.length; item++) {
 
+                Winapprove[item].forEach(element => {
+                    if (squares[element].classList.contains("X")) {
+                        xWinnerVal++;
+                    } else if (squares[element].classList.contains("O")) {
+                        oWinnerVal++;
+                    }
+                });
+                if (xWinnerVal == 3 || oWinnerVal == 3) {
+                    return 1;
+                }
+                xWinnerVal = 0;
+                oWinnerVal = 0;
+                if (item < Winapprove.length - 1) {
+                    continue;
+                }
+                return 0;
+
+            }
+        }
 
 
     });
 
     bton.addEventListener('click', (event) => {
-        squares.forEach((sq) => sq.innerHTML = "");
+        squares.forEach((sq) => location.reload());
     })
 });
